82903599f7e398c024a973a75e2a292e6e31d5f0
yokojimakojima-coder <yokojima.kojima@gmail.com>
2026-04-22 07:21:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,10 @@
-// シンプルキャッシュ版 Service Worker
-
-const CACHE_NAME = "kojibado-cache-v1";
-const URLS_TO_CACHE = [
-  "index.html",
-  "players.html",
-  "attendance.html",
-  "style.css",
-  "common.js",
-  "icon-192.png",
-  "icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+self.addEventListener("install", (e) => self.skipWaiting());
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(fetch(e.request));
 });
